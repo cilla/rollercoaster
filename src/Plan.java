67e555cabfa5bcf7ade9d8c14f7ab8908791bcf1
Cilla <Cilla@192.168.1.37>
2011-05-01 16:15:17 +0200

    Plan, RollerCoasterPark and Main (excerice 4)

---
+++ src/Plan.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+
+public class Plan {
+
+	public Plan(RollerCoasterPark park, String[] name) {
+		
+	}
+	
+	public Plan(RollerCoasterPark park, ArrayList<RollerCoaster>  rcList) {
+		
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public int computeTime(double velocity) {
+		
+	}
+	
+	public Plan optimizePlan() {
+		
+	}
+	
+	
+}
---
+++ src/RollerCoasterPark.java
@@ -0,0 +1,29 @@
+
+public class RollerCoasterPark {
+
+	public RollerCoasterPark() {
+		
+	}
+	
+	public void init() {
+//		this.name = "Liseberg";
+//		this.rollerCoasters = new ArrayList<String>();
+//		rollerCoasters.add("Hangover")
+//		rollerCoasters.add("Flumride")
+//		rollerCoasters.add("Colorado")
+//		rollerCoasters.add("Rabalder")
+	}
+	
+	public String getName() {
+		
+	}
+	
+	public RollerCoaster find(String name) {
+		
+	}
+	
+	public int nbrRollerCoasters() {
+		
+	}
+	
+}
