4a91185f35ef6d9d7e556f5fb699a2ec5153feef
Cilla <Cilla@192.168.1.37>
2011-05-01 15:55:41 +0200

    RollerCoaster.java template

---
+++ src/RollerCoaster.java
@@ -0,0 +1,20 @@
+
+public class RollerCoaster {
+
+	public RollerCoaster(String name, int time, double x, double y) {
+		
+	}
+	
+	public String getName() {
+		
+	}
+	
+	public int getTime() {
+		
+	}
+	
+	public double distanceTo(RollerCoaster other) {
+		
+	}
+	
+}
