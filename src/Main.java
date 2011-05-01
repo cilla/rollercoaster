0e37f3b873f5c7bd9693016df15a8d9f975c3895
Cilla <Cilla@192.168.1.37>
2011-05-01 21:09:14 +0200

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,1 @@
 
-public class Main {
-
-	public static void main(String[] args) {
-		
-	}
-	
-}
--- src/Plan.java
+++ src/Plan.java
@@ -1,27 +1,14 @@
 import java.util.ArrayList;
 
 
-public class Plan {
+public class Plan 
 
-	public Plan(RollerCoasterPark park, String[] name) {
-		
-	}
+	public Plan(RollerCoasterPark park, String[] name) 
 	
-	public Plan(RollerCoasterPark park, ArrayList<RollerCoaster>  rcList) {
-		
-	}
+	public Plan(RollerCoasterPark park, ArrayList<RollerCoaster>  rcList) 
 	
-	public String toString() {
-		
-	}
+	public String toString() 
 	
-	public int computeTime(double velocity) {
-		
-	}
+	public int computeTime(double velocity) 
 	
-	public Plan optimizePlan() {
-		
-	}
-	
-	
-}
+	public Plan optimizePlan() 
